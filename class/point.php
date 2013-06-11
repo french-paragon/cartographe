f3667b3f625bc866546ac6a22e629e4ac8ca15aa
paragon <laurent.valentin@famillejospin.ch>
2013-06-11 19:14:18 +0200

    Deux fichiers ajouté:
    La classe point.php inclut en plus de coordonnée des infos sur un modèle et des paramètre éventuellement associcé qui permettront l'affichage du point en question.
    
    conf.php qui charge essentiellement une table de hachage où peuvent se stocker différent paramètres. Cela permet de gérer des constante, il faudra une classe qui gère les paramètres de la base de donnée.

---
+++ class/point.php
@@ -0,0 +1,50 @@
+<?php
+    class point extends coordonnee
+    {
+		/* Vars */
+		protected $modelName;
+		protected $modelParams;
+		protected $message;
+		
+		/* Constantes */
+		const DELFAUTMODELNAME = "delfaut";
+		const USEDELFAUTMODELMESSAGE = -1;
+		const NOMODELFOUNDMESSAGE = -2;
+		
+		/*functions*/
+		
+		function __construct($cords, $pmodelName, $pmodelParams) {
+			parent::__construct($cords);
+			
+			if ($conf && isset($conf_values['rootFolder'])) { //si la configuration en chargée.
+			
+				$rootF = $conf_values['rootFolder']; 
+				
+			} else { //sinon on parcourt les dossiers en chemin relatif
+			
+				$rootF = '';
+				
+			}
+			
+			if (file_exists($rootF.'points_models/'.$pmodelName)) { //si le dossier du model existe
+					
+				$this->modelName = $pmodelName;
+					
+			} else if (file_exists($rootF.'points_models/'.self::DELFAUTMODELNAME)) { //sinon si un modèle par défaut existe.
+				
+				$this->modelName = self::DELFAUTMODELNAME;
+				$this->message = self::USEDELFAUTMODELMESSAGE;
+				
+			} else { //sinon
+				
+				$this->modelName = self::DELFAUTMODELNAME; //on donne le nom par défaut
+				$this->message = self::NOMODELFOUNDMESSAGE; //et on indique que le modèle n'existe pas
+					
+			}
+			
+			$this->modelParams = $pmodelParams;
+			
+		}
+		
+	}
+?>
---
+++ conf.php
@@ -0,0 +1,9 @@
+<?php 
+
+$conf = true; //indique que le fichier de configuration a été inclut.
+
+$conf_values = array(); //tableau qui contiendra les variable de configuration.
+
+$conf_values['rootFolder'];
+
+?>
