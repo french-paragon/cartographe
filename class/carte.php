faf13a9e0c13a85187dfafdec5fcba691c4a9de4
paragon <laurent.valentin@famillejospin.ch>
2013-06-12 20:46:03 +0200

    Une nouvelle classe pour gérer les carte WIP: carte.php

---
+++ class/carte.php
@@ -0,0 +1,23 @@
+<?php
+    class carte
+    {
+		protected $name;
+		protected $image_fond;
+		protected $deco_style;
+		protected $x_size;
+		protected $y_size;
+		protected $description;
+		protected $message;
+		 
+		public function __construct($pName, $pImage_fond, $pDeco_style, $pX_size, $pY_size, $pDecription){
+			
+			$this->name = $pName;
+			
+			if ()
+			
+		}
+		
+		
+		
+	}
+?>
