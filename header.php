6c6caa88aca6e0b033a75a88e2b37c8951dd2fde
paragon <laurent.valentin@famillejospin.ch>
2013-06-28 22:34:43 +0200

    petites corrections sur header.php
     Changes to be committed:
    	modified:   header.php

--- header.php
+++ header.php
@@ -1,7 +1,11 @@
 <?php
 
+session_start(); //démarrage de session, elle servira à stocker la configuration + d'autre choses.
+
 spl_autoload_register(function ($class) {
     include 'class/'.$class.'.php';
 });
+
+include('conf.php');
 
 ?>
