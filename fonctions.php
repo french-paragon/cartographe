19bd01ae8946878cfb9e4b9795e979b9644c9f89
french-paragon <french.paragon@gmail.com>
2014-02-03 15:34:00 +0100

    New functions collections.

---
+++ fonctions.php
@@ -0,0 +1,30 @@
+<?php
+
+	function url_exists($url) {
+    
+		$headers = @get_headers($url);
+		if(strpos($headers[0],'200')===false)return false;
+		return true;
+	
+	}
+
+	function prepareSave($texte){
+	
+		$retour = preg_replace('#<script(.+)</script>#i', '', $texte);
+		$retour = str_replace('\\\'', '&apos;', $retour);
+		$retour = preg_replace('#,#', '&#44;', $retour);
+		
+		return $retour;
+		
+	}
+	
+	function prepareSaveSoft($texte){
+	
+		$retour = preg_replace('#<script(.+)</script>#i', '', $texte);
+		$retour = str_replace('\\\'', '&apos;', $retour);
+		
+		return $retour;
+		
+	}
+
+?>
